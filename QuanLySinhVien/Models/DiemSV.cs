3394446d61ca2533b59736fbbf1cec506f749a41
Hiếu Bùi <hieubuim012@gmail.com>
2020-12-12 19:30:28 +0700

    update

--- QuanLySinhVien/Models/DiemSV.cs
+++ QuanLySinhVien/Models/DiemSV.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -16,9 +17,11 @@ namespace QuanLySinhVien.Models
         public string MaSV { get; set; }
 
         [BsonElement("diemky1")]
+        [Range(typeof(float), "0", "100")]
         public float DiemKy1 { get; set; }
 
         [BsonElement("diemky2")]
+        [Range(typeof(float), "0", "100")]
         public float DiemKy2 { get; set; }
 
         [BsonElement("ketqua")]
